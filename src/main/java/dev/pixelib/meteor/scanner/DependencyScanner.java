d5f1bec251f3c6e5b9bc0f416ffb6a5ea15f8baf
Duckelekuuk <duckelekuuk@gmail.com>
2024-08-03 00:01:13 +0200

    WIP scanning dependency count

--- src/main/java/dev/pixelib/meteor/scanner/DependencyScanner.java
+++ src/main/java/dev/pixelib/meteor/scanner/DependencyScanner.java
@@ -23,20 +23,39 @@ public class DependencyScanner {
         this.dependencyCount = new HashMap<>();
     }
 
-    public void findAllComponents() {
-    }
 
-    private void findAllAnnotatedClasses() {
+    public void findAllComponents() {
         Set<Class<?>> classes = this.reflections.getTypesAnnotatedWith(Component.class);
 
         classes.forEach(this::findAllMethodComponents);
+    }
+
+    public void countDependencies() {
+        for (AbstractScanResult value : results.values()) {
+            value.getDependencies().stream().map()
+        }
+    }
+
+    private int getDependencyCount(AbstractScanResult result) {
+        if (result == null) {
+            throw new IllegalStateException("Cannot get dependency count for null result");
+        }
+        int count = 0;
+        for (Class<?> dependency : result.getDependencies()) {
+            count += dependencyCount.computeIfAbsent(dependency, t -> getDependencyCount(results.get(t)));
+        }
+
+        return count;
     }
 
     private void findAllMethodComponents(Class<?> clazz) {
         for (Method declaredMethod : clazz.getDeclaredMethods()) {
             if (declaredMethod.isAnnotationPresent(Component.class)) {
                 MethodComponentResult methodComponentResult = new MethodComponentResult(clazz, declaredMethod);
-                
+
+                results.put(methodComponentResult.getResultType(), methodComponentResult);
+
+                findAllMethodComponents(methodComponentResult.getResultType());
             }
         }
     }
